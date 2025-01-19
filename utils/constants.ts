99d78d5ac240a6d51d7bcb29a882091fcd7d2ca8
Manish <manishguhe301@gmail.com>
2025-01-19 15:22:20 +0530

    feat: assets added for features + added constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -13,3 +13,31 @@ export const COMPANIES_HERO_SECTION_IMAGES = [
   '/assets/globalbank.svg',
   '/assets/nietz.svg',
 ]
+
+export const FEATURES_DATA = [
+  {
+    src: '/assets/features/chat.svg',
+    title: 'Share team inboxes',
+    description: 'Whether you have a team of 2 or 200, our shared team inboxes keep everyone on the same page and in the loop.'
+  }, {
+    src: '/assets/features/thunder.svg',
+    title: 'Deliver instant answers',
+    description: 'An all-in-one customer service platform that helps you balance everything your customers need to be happy.'
+  }, {
+    src: '/assets/features/manage.svg',
+    title: 'Manage your team with reports',
+    description: 'Measure what matters with Untitled’s easy-to-use reports. You can filter, export, and drilldown on the data in a couple clicks.'
+  }, {
+    src: '/assets/features/connect.svg',
+    title: 'Connect with customers',
+    description: 'Solve a problem or close a sale in real-time with chat. If no one is available, customers are seamlessly routed to email without confusion.'
+  }, {
+    src: '/assets/features/cmd.svg',
+    title: 'Connect the tools you already use',
+    description: 'Explore 100+ integrations that make your day-to-day workflow more efficient and familiar. Plus, our extensive developer tools.'
+  }, {
+    src: '/assets/features/diff.svg',
+    title: 'Our people make the difference',
+    description: 'We’re an extension of your customer service team, and all of our resources are free. Chat to our friendly team 24/7 when you need help.'
+  },
+]
